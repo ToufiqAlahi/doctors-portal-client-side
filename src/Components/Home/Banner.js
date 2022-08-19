5793f104b399b7c5058a5b24fb34712ae8325fa2
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-19 23:57:47 +0600

    added banner background image
    added banner

---
+++ src/Components/Home/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import chair from '../../assets/images/chair.png'
+
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen lg:bg-banner-background">
+            <div class="hero-content flex-col gap-2 lg:flex-row-reverse">
+                <img src={chair} class="lg:max-w-xl max-w-sm rounded-lg shadow-2xl" alt='chair' />
+                <div>
+                    <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p class="py-6 lg:pr-3">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary hover:text-white font-bold ">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
