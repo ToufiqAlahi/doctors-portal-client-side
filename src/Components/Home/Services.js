e24608e4121bb04bd4bb00bce0a205cc5cf0650e
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-25 13:56:33 +0600

    added card data

--- src/Components/Home/Services.js
+++ src/Components/Home/Services.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import ServiceCard from './ServiceCard';
+import Fluoride from '../../assets/images/fluoride.png'
+import Cavity from '../../assets/images/cavity.png'
+import Whitening from '../../assets/images/whitening.png'
 
 const Services = () => {
+    const cardData = [
+        {
+            _id: 1,
+            cardImg: Fluoride,
+            CardTitle: 'Fluoride Treatment',
+            cardDescription: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the'
+        },
+
+        {
+            _id: 2,
+            cardImg: Cavity,
+            CardTitle: 'Cavity Filling',
+            cardDescription: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the'
+        },
+        {
+            _id: 3,
+            cardImg: Whitening,
+            CardTitle: "Teeth Whitening",
+            cardDescription: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+        },
+    ]
     return (
         <div>
-            
+
+
+            <div>
+                
+            </div>
+
+
+            <div>
+                {
+
+
+                }
+            </div>
         </div>
     );
 };
