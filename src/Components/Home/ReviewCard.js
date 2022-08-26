b476ad6e1fbc32252fe3ed4035711bb2470b7d30
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-26 15:17:50 +0600

    Update ReviewCard.js

--- src/Components/Home/ReviewCard.js
+++ src/Components/Home/ReviewCard.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const ReviewCard = () => {
+const ReviewCard = ({ review }) => {
     return (
-        <div>
-            
+        <div className='border shadow-xl rounded-3xl p-10'>
+            <div>
+                <p>{review.text} </p>
+            </div>
+
+            <div className='w-3/4 grid grid-cols-2 items-center mt-7'>
+                <div class="">
+                    <img class="mask mask-circle border-4 border-secondary rounded-full " src={review.img} alt=""></img>
+
+                </div>
+
+                <div>
+                    <h3 className='font-bold text-xl'>{review.name}</h3>
+                    <p class=" text-black py-2">{review.address}</p>
+                </div>
+            </div>
         </div>
+        //     <div class=" min-h-screen bg-base-200"> 
+        // </div>
     );
 };
 
