96964fbd658103ef50da2daa527dd1a84dd22461
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-26 08:56:58 +0600

    Update Info.js

--- src/Components/Home/Info.js
+++ src/Components/Home/Info.js
@@ -8,9 +8,9 @@ import quote from '../../assets/icons/quote.svg'
 const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 my-20 text-center '>
-            <InfoCard cardTitle="Opening Hours" cardTexts="Lorem Ipsum is simply dummy text of the pri" bgClass="bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary" img={clock}></InfoCard>
-            <InfoCard cardTitle="Visit our location" cardTexts="Brooklyn, NY 10036, United States" bgClass="bg-neutral" img={marker}></InfoCard>
-            <InfoCard cardTitle="Contact us now" cardTexts="+000 123 456789" bgClass="bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary" img={phone}></InfoCard>
+            <InfoCard cardTitle="Opening Hours" cardTexts="Lorem Ipsum is simply dummy text of the pri" bgclassName="bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary" img={clock}></InfoCard>
+            <InfoCard cardTitle="Visit our location" cardTexts="Brooklyn, NY 10036, United States" bgclassName="bg-neutral" img={marker}></InfoCard>
+            <InfoCard cardTitle="Contact us now" cardTexts="+000 123 456789" bgclassName="bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary" img={phone}></InfoCard>
         </div>
     );
 };
