894023672e90aba82082041f1b5668ad69ff4e26
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-26 10:50:26 +0600

    Update Banner.js

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -3,7 +3,7 @@ import chair from '../../assets/images/chair.png'
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen max-w-screen lg:bg-banner-background ">
+        <div className=" px-4 lg:px-10 hero min-h-screen max-w-screen lg:bg-banner-background ">
             <div className="hero-content max-w-screen  flex-col lg:flex-row-reverse justify-between ">
                 <img src={chair} className="lg:max-w-xl lg:ml-10 max-w-xs rounded-lg shadow-2xl" alt='chair' />
                 <div>
