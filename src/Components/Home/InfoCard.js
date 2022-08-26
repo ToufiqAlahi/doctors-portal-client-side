e7c80b857aca371d937da786ae2a8ed182a896b8
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-26 08:57:14 +0600

    Update InfoCard.js

--- src/Components/Home/InfoCard.js
+++ src/Components/Home/InfoCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const InfoCard = ({img, cardTitle, cardTexts, bgClass}) => {
+const InfoCard = ({ img, cardTitle, cardTexts, bgClass }) => {
     return (
-        <div class={` card lg:card-side bg-base-100 shadow-xl text-white px-4 ${bgClass} `}>
+        <div className={` card lg:card-side bg-base-100 shadow-xl text-white px-4 ${bgClass} `}>
             <figure className='p-5'>
                 <img src={img} alt="Album"></img>
             </figure>
-            <div class="card-body">
-                <h2 class="card-title">{cardTitle}</h2>
+            <div className="card-body">
+                <h2 className="card-title">{cardTitle}</h2>
                 <p>{cardTexts}</p>
-                {/* <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Listen</button>
+                {/* <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Listen</button>
                 </div> */}
             </div>
         </div>
