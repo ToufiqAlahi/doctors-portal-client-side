af54ced955cbf6eb9c2fe32f7cef528fef513e28
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-26 09:03:01 +0600

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import DentalCard from './DentalCard';
+import HomeAppointment from './HomeAppointment';
 import Info from './Info';
 import Services from './Services';
 
@@ -12,6 +13,7 @@ const Home = () => {
             <Info></Info>
             <Services></Services>
             <DentalCard></DentalCard>
+            <HomeAppointment></HomeAppointment>
         </div>
     );
 };
