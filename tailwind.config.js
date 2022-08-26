6bc4f917c6cfec915bc524294f06b56a7836dc69
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-26 09:45:45 +0600

    added  background images for appointment component

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'banner-background': "url('/src/assets/images/bg.png')",
+        'appointment-background': "url('/src/assets/images/appointment.png')",
       }
     },
   },
