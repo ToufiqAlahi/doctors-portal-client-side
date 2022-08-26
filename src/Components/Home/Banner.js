6077236cc79b3d16cb344610a08fea5e0d9d9e77
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-26 08:56:49 +0600

    Update Banner.js

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -3,13 +3,13 @@ import chair from '../../assets/images/chair.png'
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen max-w-screen lg:bg-banner-background ">
-            <div class="hero-content max-w-screen  flex-col lg:flex-row-reverse justify-between ">
-                <img src={chair} class="lg:max-w-xl lg:ml-10 max-w-xs rounded-lg shadow-2xl" alt='chair' />
+        <div className="hero min-h-screen max-w-screen lg:bg-banner-background ">
+            <div className="hero-content max-w-screen  flex-col lg:flex-row-reverse justify-between ">
+                <img src={chair} className="lg:max-w-xl lg:ml-10 max-w-xs rounded-lg shadow-2xl" alt='chair' />
                 <div>
-                    <h1 class="text-5xl font-bold mt-14 lg:mr-10 lg:mt-0">Your New Smile Starts Here</h1>
-                    <p class="py-10 ">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary hover:text-white font-bold ">Get Started</button>
+                    <h1 className="text-5xl font-bold mt-14 lg:mr-10 lg:mt-0">Your New Smile Starts Here</h1>
+                    <p className="py-10 ">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary text-white font-bold ">Get Started</button>
                 </div>
             </div>
         </div>
