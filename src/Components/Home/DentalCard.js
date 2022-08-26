0d60c3772f5b9c93827da9162b55085db3dd3524
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-26 08:58:33 +0600

    Create DentalCard.js

---
+++ src/Components/Home/DentalCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DentalImg from '../../assets/images/treatment.png'
+import './module.dental.css'
+
+const DentalCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DentalCard;
