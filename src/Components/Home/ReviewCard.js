86f9e309d5d15fa555a0ce9e69439c65dee83435
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-27 11:51:06 +0600

    replaced class with className

--- src/Components/Home/ReviewCard.js
+++ src/Components/Home/ReviewCard.js
@@ -8,18 +8,18 @@ const ReviewCard = ({ review }) => {
             </div>
 
             <div className='w-3/4 grid grid-cols-2 items-center mt-7'>
-                <div class="">
-                    <img class="mask mask-circle border-4 border-secondary rounded-full " src={review.img} alt=""></img>
+                <div className="">
+                    <img className="mask mask-circle border-4 border-secondary rounded-full " src={review.img} alt=""></img>
 
                 </div>
 
                 <div>
                     <h3 className='font-bold text-xl'>{review.name}</h3>
-                    <p class=" text-black py-2">{review.address}</p>
+                    <p className=" text-black py-2">{review.address}</p>
                 </div>
             </div>
         </div>
-        //     <div class=" min-h-screen bg-base-200"> 
+        //     <div className=" min-h-screen bg-base-200"> 
         // </div>
     );
 };
