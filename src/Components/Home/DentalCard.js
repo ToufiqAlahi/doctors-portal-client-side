ab465e9e9cbab4789dca3ce5a698919c9a3590fc
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-27 11:51:41 +0600

    replaced class with className

--- src/Components/Home/DentalCard.js
+++ src/Components/Home/DentalCard.js
@@ -5,13 +5,13 @@ import './module.dental.css'
 const DentalCard = () => {
     return (
         <div className='px-4 lg:px-10 mb-32 lg:bg-banner-background'>
-            <div class="lg:mx-40 grid grid-cols-1 md:grid-cols-2 gap-20  justify-center text-left items-center lg:text-left">
+            <div className="lg:mx-40 grid grid-cols-1 md:grid-cols-2 gap-20  justify-center text-left items-center lg:text-left">
                 <figure className='mx-6'>
                     <img className='border rounded-xl' src={DentalImg} alt="Album"></img>
                 </figure>
-                <div class="align-middle text-left ">
-                    <h2 class=" text-5xl font-bold w-full">Exceptional Dental Care, on Your Terms</h2>
-                    <p class="mt-7 text-justify">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                <div className="align-middle text-left ">
+                    <h2 className=" text-5xl font-bold w-full">Exceptional Dental Care, on Your Terms</h2>
+                    <p className="mt-7 text-justify">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                     <button className="mt-7 btn btn-primary bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary text-white font-bold ">Get Started</button>
                 </div>
             </div>
--- src/Components/Home/HomeAppointment.js
+++ src/Components/Home/HomeAppointment.js
@@ -5,14 +5,14 @@ import './module.HomeAppointment.css'
 const HomeAppointment = () => {
     return (
         <div className='lg:mt-72 mb-32 bg-appointment-background w-full p-4 lg:py-0 lg:px-44'>
-            <div class=" md:grid  md:grid-cols-2  text-left items-center md:text-left">
+            <div className=" md:grid  md:grid-cols-2  text-left items-center md:text-left">
                 <figure className='hidden lg:block'>
                     <img className=' mt-[-120px]' src={Doctor} alt="Album"></img>
                 </figure>
-                <div class="align-middle text-left ">
+                <div className="align-middle text-left ">
                     <h3 className='text-xl text-secondary text-center md:text-left font-bold mb-2' > Appointment</h3>
-                    <h2 class=" text-4xl font-bold w-full text-white">Make an appointment Today</h2>
-                    <p class="mt-7  text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                    <h2 className=" text-4xl font-bold w-full text-white">Make an appointment Today</h2>
+                    <p className="mt-7  text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                     <button className="mt-7 btn btn-primary bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary text-white font-bold ">Get Started</button>
                 </div>
             </div>
