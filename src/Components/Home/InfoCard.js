9aed5f73a24bdbdf6f7069e28980c402b84d0217
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-28 09:46:06 +0600

    Update InfoCard.js

--- src/Components/Home/InfoCard.js
+++ src/Components/Home/InfoCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const InfoCard = ({ img, cardTitle, cardTexts, bgclassName }) => {
     return (
-        <div className={` card lg:card-side bg-base-100 shadow-xl text-white px-4 ${bgclassName} `}>
+        <div className={` text-left card lg:card-side bg-base-100 shadow-xl text-white px-4 ${bgclassName} `}>
             <figure className='p-5'>
                 <img src={img} alt="Album"></img>
             </figure>
