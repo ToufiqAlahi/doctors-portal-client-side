5f82f441a5fa48014ea06c0d6b406a04f547e9c3
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-08-28 09:45:49 +0600

    Update Banner.js

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -3,12 +3,12 @@ import chair from '../../assets/images/chair.png'
 
 const Banner = () => {
     return (
-        <div className=" px-4 lg:px-10 hero min-h-screen max-w-screen lg:bg-banner-background ">
-            <div className="hero-content max-w-screen  flex-col lg:flex-row-reverse justify-between ">
-                <img src={chair} className="lg:max-w-xl lg:ml-10 max-w-xs rounded-lg shadow-2xl" alt='chair' />
+        <div className="  hero  lg:min-h-screen items-center max-w-screen lg:bg-banner-background ">
+            <div className="hero-content max-w-screen flex-col lg:flex-row-reverse ">
+                <img src={chair} className="lg:max-w-2xl max-w-full rounded-lg shadow-2xl" alt='chair' />
                 <div>
-                    <h1 className="text-5xl font-bold mt-14 lg:mr-10 lg:mt-0">Your New Smile Starts Here</h1>
-                    <p className="py-10 ">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="text-5xl font-bold mt-8">Your New Smile Starts Here</h1>
+                    <p className="my-10 ">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button className="btn btn-primary bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary text-white font-bold ">Get Started</button>
                 </div>
             </div>
