929a995aab4269b4823077bf6f1f67a080400a86
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-10 00:18:52 +0600

    Update service.js

--- src/Appointment/service.js
+++ src/Appointment/service.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const Service = ({ service }) => {
+const Service = ({ service}) => {
 
-    const { name, slots } = service;
-    
+    const { name} = service;
+
     return (
-        <div className= "  border-gray-100 border lg:max-w-lg mx-auto card w-96 bg-base-100 shadow-xl">
+        <div className="  border-gray-100 border lg:max-w-lg mx-auto card w-96 bg-base-100 shadow-xl">
             <div className="card-body  ">
                 <h2 className="text-xl font-semibold text-secondary text-center">{name}</h2>
-                
-                
             </div>
         </div>
     );
