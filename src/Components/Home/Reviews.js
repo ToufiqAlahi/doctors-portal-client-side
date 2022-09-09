aa66a0c0d297e9ffcbe817f2dc012ea630cee9d5
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-09 17:40:37 +0600

    added css

--- src/Components/Home/Reviews.js
+++ src/Components/Home/Reviews.js
@@ -4,7 +4,7 @@ import Angella from '../../assets/images/people2.png'
 import Cody from '../../assets/images/people3.png'
 import ReviewCard from './ReviewCard';
 import './module.Review.css';
-// import leftQuote from '../../assets/images/left-quote.svg';
+import leftQuote from '../../assets/images/left-quote.svg';
 
 const Reviews = () => {
     const reviews = [
@@ -33,13 +33,13 @@ const Reviews = () => {
     return (
         <div className='px-4 lg:px-10 lg:min-h-screen mt-32 justify-center'>
 
-            <div className='mb-20 review-container  flex'>
-                <div className='text-left' >
+            <div className='lg:mb-20 review-container  flex justify-between'>
+                <div className='text-left w-2/4 lg:w-full' >
                     <h3 className='text-xl text-secondary uppercase font-bold mb-2' > Testimonial</h3>
-                    <h2 className='text-3xl uppercase mb-20'>What Our Patients Says</h2>
+                    <h2 className='lg:text-3xl text-2xl uppercase mb-20'>What Our Patients Says</h2>
                 </div>
                 <div>
-                    {/* <img src={leftQuote} alt=""></img> */}
+                    <img className='w-24 md:w-48' src={leftQuote} alt=""></img>
                 </div>
             </div>
 
