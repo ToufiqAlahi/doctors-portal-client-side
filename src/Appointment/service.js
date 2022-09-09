c7044e5a7b224f0ac8a0b59ff5330c05ed8fb8da
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-09 19:14:55 +0600

    Update service.js

--- src/Appointment/service.js
+++ src/Appointment/service.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const service = () => {
+const Service = ({ service }) => {
+
+    const { name, slots } = service;
+    
     return (
-        <div>
-            
+        <div className= "  border-gray-100 border lg:max-w-lg mx-auto card w-96 bg-base-100 shadow-xl">
+            <div className="card-body  ">
+                <h2 className="text-xl font-semibold text-secondary text-center">{name}</h2>
+                
+                
+            </div>
         </div>
     );
 };
 
-export default service;
+export default Service;
