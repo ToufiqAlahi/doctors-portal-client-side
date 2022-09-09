3783625ed4b02069b309f3fcff635ae50f7a3acf
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-09 19:14:53 +0600

    Update AvailableAppointments.js

--- src/Appointment/AvailableAppointments.js
+++ src/Appointment/AvailableAppointments.js
@@ -2,19 +2,28 @@ import React from 'react';
 import { format } from 'date-fns';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Service from './Service';
+
 const AvailableAppointments = ({ date }) => {
     const [services, setServices] = useState([]);
     useEffect(() => {
-        fetch(services.json)
-            .then(res => res.json)
-        .then (data => setServices)
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
     },[])
     
     return (
-        <div className='h-screen text-center  my-16'>
+        <div className='h-screen text-center my-48'>
             <h3 className=' text-secondary text-2xl '>Available Services on: {format(date, 'PP')}</h3>
             <p className='text-xl'>Please select a service.</p>
-            
+            <div className='mt-24 grid grid-cols-1 lg:grid-cols-3 mg:grid-cols-2 gap-5 justify-center items-center'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
