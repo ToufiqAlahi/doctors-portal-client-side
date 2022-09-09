85d660246efce7febc3fd61d31907b614d1f603e
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-09 17:40:09 +0600

    relocated footer

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../Shared/Footer';
 import Banner from './Banner';
 import Contact from './Contact';
 import DentalCard from './DentalCard';
@@ -18,6 +19,7 @@ const Home = () => {
             <HomeAppointment></HomeAppointment>
             <Reviews></Reviews>
             <Contact></Contact>
+            <Footer></Footer>
         </div>
     );
 };
