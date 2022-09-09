fef4d30f4a12818bbfd2378490dadae9cce9b0c4
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-09 18:16:42 +0600

    added calendar

--- src/Appointment/AppointmentBanner.js
+++ src/Appointment/AppointmentBanner.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../assets/images/chair.png'
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { format } from 'date-fns';
+
+
 const AppointmentBanner = () => {
+    const [date, setDate] = useState(new Date())
     return (
         <div>
-            <div className="  hero  lg:min-h-screen items-center max-w-screen lg:bg-banner-background ">
-                <div className="hero-content max-w-screen flex-col lg:flex-row-reverse ">
-                    <img src={chair} className="lg:max-w-2xl max-w-full rounded-lg shadow-2xl" alt='chair' />
-                    <div>
-                        <h1 className="text-5xl font-bold mt-8">Your New Smile Starts Here</h1>
-                        <p className="my-10 ">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary text-white font-bold ">Get Started</button>
+            <div className="   hero lg:min-h-screen items-center max-w-screen lg:bg-banner-background ">
+                <div className="hero-content max-w-screen flex-col lg:flex-row-reverse justify-around ">
+                    <img src={chair} className="lg:max-w-xl lg:ml-40 max-w-full  rounded-lg shadow-2xl" alt='chair' />
+                    <div className=''>
+                        <DayPicker
+                            className=' shadow-2xl p-3 border-gray-800  rounded-xl'
+                            mode="single"
+                            selected={date}
+                            onSelect={setDate}
+                        />
+                            
+                       <p>Date selected: { format(date, 'PP' ) }</p>
+                       
                     </div>
                 </div>
             </div>
+            
         </div>
     );
 };
