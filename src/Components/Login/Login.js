8ff0403f58ed2b675ea40eac3560d9546bcd0d39
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-10 13:04:07 +0600

    Update Login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,45 @@
 import React from 'react';
+// import { getAuth, signInWithEmailAndPassword, signInWithGoogle , signOut } from 'firebase/auth';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (user) {
+        console.log(user);
+    }
+    const onSubmit = (data) => {
+        console.log(data)
+    
+    
+    };
+
+    
     return (
-        <div>
-            <h1> Login </h1> 
-            <h1> Login </h1> 
-            <h1> Login </h1> 
-            <h1> Login </h1> 
-            <h1> Login </h1> 
-            <h1> Login </h1> 
-            <h1> Login </h1> 
+        <div className='flex justify-center items-center h-screen'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-2xl font-bold ">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input {...register("firstName", { required: true })} />
+                        {errors.firstName?.type === 'required' && "First name is required"}
+
+                        <input {...register("lastName", { required: true })} />
+                        {errors.lastName && <p>Last name is required</p>}
+
+                        <input {...register("mail", { required: "Email Address is required" })} />
+                        <p>{errors.mail?.message}</p>
+
+                        <input type="submit" />
+                    </form>
+                    <div className="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        className="btn btn-outline ">Continue with Google</button>
+                </div>
+            </div>
         </div>
     );
 };
