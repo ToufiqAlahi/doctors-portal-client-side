c303e8e3e40591b34a165ef20d1c240ee6215e67
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-12 19:59:49 +0600

    Update Login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
+import google from '../../../src/assets/icons/social/google.png'
 
 const Login = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
@@ -12,32 +13,94 @@ const Login = () => {
     }
     const onSubmit = (data) => {
         console.log(data)
-    
-    
+
+
     };
 
-    
+
     return (
         <div className='flex justify-center items-center h-screen'>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="text-center text-2xl font-bold ">Login</h2>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <input {...register("firstName", { required: true })} />
-                        {errors.firstName?.type === 'required' && "First name is required"}
 
-                        <input {...register("lastName", { required: true })} />
-                        {errors.lastName && <p>Last name is required</p>}
 
-                        <input {...register("mail", { required: "Email Address is required" })} />
-                        <p>{errors.mail?.message}</p>
+                        <div className="form-control w-full max-w-xs ">
+                            <label className="label">
+                                <span className="label-text">Email</span>
 
-                        <input type="submit" />
+                            </label>
+                            <input type="email"
+                                placeholder="Your Email" className="input input-bordered w-full max-w-xs"
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is Required'
+
+                                    },
+                                    pattern: {
+                                        value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                        message: 'Please Provide a valid Email'
+                                    }
+                                })}
+
+                            />
+
+                            <label className="label">
+                                {errors.email?.type === 'required' && <span className="label-text-alt text-red-600">{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-600">{errors.email.message}</span>}
+
+
+                            </label>
+                        </div>
+                        <div className="form-control w-full max-w-xs ">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+
+                            </label>
+                            <input type="password"
+                                placeholder="Password" className="input input-bordered w-full max-w-xs"
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: 'Password is Required'
+
+                                    },
+                                    minLength: {
+                                        value: 8,
+                                        message: 'Password Must be at least 8 characters' // JS only: <p>error message</p> TS only support string
+                                    },
+                                    maxLength: {
+                                        value: 20,
+                                        message: 'Password Must be between 8-20 characters' // JS only: <p>error message</p> TS only support string
+                                    },
+                                    pattern: {
+                                        value: /^(\S)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])[a-zA-Z0-9~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]{8,20}$/,
+                                        message: 'Please Provide a valid Password'
+                                    }
+                                })}
+
+                            />
+
+                            <label className="label">
+                                {errors.password?.type === 'required' && <span className="label-text-alt text-red-600">{errors.password.message}</span>}
+                                {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-600">{errors.password.message}</span>}
+                                {errors.password?.type === 'maxLength' && <span className="label-text-alt text-red-600">{errors.password.message}</span>}
+                                {errors.password?.type === 'pattern' && <span className="label-text-alt text-red-600">{errors.password.message}</span>}
+                            </label>
+                        </div>
+                        <input />
+
+                       
+                        <input className="btn w-full max-w-xs text-white" value="Login" type="submit" />
                     </form>
                     <div className="divider">OR</div>
                     <button
                         onClick={() => signInWithGoogle()}
-                        className="btn btn-outline ">Continue with Google</button>
+                        className="btn btn-outline ">
+                        <img className='w-8 mr-2  ' src={google} alt="" />
+                        Continue with Google</button>
                 </div>
             </div>
         </div>
