f6a008bc1c2ad2b0e08f84f65854089fc090d875
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-13 18:40:37 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ import Login from './Components/Login/Login';
 import Appointment from './Appointment/Appointment';
 import SignUp from './Components/Login/SignUp';
 import RequireAuth from './Components/Login/RequireAuth';
+import Loading from './Components/Shared/Loading/Loading';
 
 function App() {
+  
   return (
     <div className='mx-auto'>
+      
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="about" element={<RequireAuth><About /></RequireAuth>} />
         <Route path="Appointment" element={<RequireAuth> <Appointment /> </RequireAuth>} />
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
