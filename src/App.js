e3f1a569631c102cc84a45820b8559d276a2dfb2
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-13 21:10:37 +0600

    added page load spinner

--- src/App.js
+++ src/App.js
@@ -4,27 +4,40 @@ import { Routes, Route } from "react-router-dom";
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Login from './Components/Login/Login';
-// import Footer from './Components/Shared/Footer';
 import Appointment from './Appointment/Appointment';
 import SignUp from './Components/Login/SignUp';
 import RequireAuth from './Components/Login/RequireAuth';
 import Loading from './Components/Shared/Loading/Loading';
+import { useState, useEffect } from 'react';
 
 function App() {
-  
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <div className='mx-auto'>
-      
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="about" element={<RequireAuth><About /></RequireAuth>} />
-        <Route path="Appointment" element={<RequireAuth> <Appointment /> </RequireAuth>} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-      </Routes>
-    </div>
+    <>
+      {
+        loading
+          ? (<Loading></Loading>)
+
+          : (<div className='mx-auto'>
+            <Navbar></Navbar>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="home" element={<Home />} />
+              <Route path="about" element={<RequireAuth><About /></RequireAuth>} />
+              <Route path="Appointment" element={<RequireAuth> <Appointment /> </RequireAuth>} />
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<SignUp />} />
+            </Routes>
+          </div>)
+      }
+    </>
   );
 }
 export default App;
