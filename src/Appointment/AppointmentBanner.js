af19785d5827af2275520bb65e5514487974e73f
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-14 21:35:41 +0600

    Update AppointmentBanner.js

--- src/Appointment/AppointmentBanner.js
+++ src/Appointment/AppointmentBanner.js
@@ -9,12 +9,12 @@ const AppointmentBanner = ({date, setDate}) => {
     
     return (
         <div>
-            <div className="   hero lg:min-h-screen items-center max-w-screen lg:bg-banner-background ">
-                <div className="hero-content max-w-screen flex-col lg:flex-row-reverse justify-around ">
+            <div className="mb-44 hero max-h-screen items-center lg:bg-banner-background">
+                <div className="lg:h-[calc(100vh-100px)] px-3 lg:px-0  flex justify-center items-center gap-24 flex-col lg:flex-row-reverse ">
                     <img src={chair} className="lg:max-w-xl lg:ml-40 max-w-full  rounded-lg shadow-2xl" alt='chair' />
                     <div className=''>
                         <DayPicker
-                            className=' shadow-2xl p-3 border-gray-800  rounded-xl'
+                            className='scale-110 lg:scale-100 text-center lg:min-h-[345px] lg:min-w-[345px] bg-opacity-25 bg-[#c3d2dc] shadow-2xl p-8 border-gray-800  rounded-2xl'
                             mode="single"
                             selected={date}
                             onSelect={setDate}
