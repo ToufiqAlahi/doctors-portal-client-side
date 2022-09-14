36f42f3c7441013908f5ce715971e126b95edcf1
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-14 21:35:45 +0600

    Update Banner.js

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -3,10 +3,11 @@ import chair from '../../assets/images/chair.png'
 
 const Banner = () => {
     return (
-        <div className="  hero  lg:min-h-screen items-center max-w-screen lg:bg-banner-background ">
-            <div className="hero-content max-w-screen flex-col lg:flex-row-reverse ">
+        <div className="mb-44 hero max-h-screen items-center lg:bg-banner-background">
+            <div className="lg:h-[calc(100vh-100px)] px-3 lg:px-0  flex justify-center items-center gap-24 flex-col lg:flex-row-reverse ">
                 <img src={chair} className="lg:max-w-2xl max-w-full rounded-lg shadow-2xl" alt='chair' />
-                <div>
+                {/* <div className='mx-12'></div> */}
+                <div className='lg:w-mytt'>
                     <h1 className="text-5xl font-bold mt-8">Your New Smile Starts Here</h1>
                     <p className="my-10 ">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button className="btn btn-primary bg-gradient-to-r from-secondary to-primary  hover:from-primary hover:to-secondary text-white font-bold ">Get Started</button>
