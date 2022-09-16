88e703b16fea228be3c46c77885ae856d78f9324
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-16 22:10:16 +0600

    update available slots by date

--- src/Appointment/AvailableAppointments.js
+++ src/Appointment/AvailableAppointments.js
@@ -9,8 +9,9 @@ import BookingModal from './BookingModal';
 const AvailableAppointments = ({ date }) => {
     const [services, setServices] = useState([]); 
     const [treatment, setTreatment] = useState({});
+    const formatedDate = format(date, 'PP');
     useEffect(() => {
-        fetch('http://localhost:5000/service')
+        fetch(`http://localhost:5000/available?date=${formatedDate}`)
             .then(res => res.json())
             .then(data => setServices(data));
     },[])
