bbdd7128003436236eb285ef784ee626efeaf562
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-19 23:36:43 +0600

    Update Dashboard.js

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
-        <div>
-            <h2>DashBoard!!!!!!</h2>
+        <div className="drawer drawer-mobile">
+            <input id="dashboard-side-bar" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+                {/* <!-- Page content here --> */}
+                <h2 className=' text-2xl font-bold text-secondary '>Welcome to your Dashboard</h2>
+                <Outlet></Outlet>
+
+
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="dashboard-side-bar" className="drawer-overlay"></label>
+                <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                    {/* <!-- Sidebar content here --> */}
+                    <li> <Link to={"/dashboard"}> blaaa </Link> </li>
+                    <li> <Link to={"/dashboard/review"}> blaaablaaablaaablaaa </Link> </li>
+                </ul>
+
+            </div>
         </div>
     );
 };
