f7c2d75bab7d467839cd13510c1347072204bd93
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-20 17:09:07 +0600

    added put method for user email

--- src/Hooks/useToken.js
+++ src/Hooks/useToken.js
@@ -5,8 +5,24 @@ const useToken = user => {
     const [token, setToken] = useState('');
 
     useEffect(() => {
+        // console.log('user inside useToken', user );
+        const email = user?.user?.email;
+        const currentUser = { email: email }
+        if (email) {
+            fetch(`http://localhost:5000/user/${email}`, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(currentUser)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log('Data inside useToken', data)
+                });
 
-        
+        }
+
     }, [user]);
 
     return [token];
