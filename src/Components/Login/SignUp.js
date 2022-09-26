8056d40156f1dfb9ae5160fd3e4f5b562f333e43
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-09-26 21:24:16 +0600

    Update SignUp.js

--- src/Components/Login/SignUp.js
+++ src/Components/Login/SignUp.js
@@ -37,7 +37,7 @@ const SignUp = () => {
     const onSubmit = async (data) => {
         
         await createUserWithEmailAndPassword(data.email, data.password);
-        await updateProfile({ displayName: data.name});
+        await updateProfile({ displayName: data.name });
         console.log('update done');
         // navigate('/Appointment');
     };
