923ea0a46b6d41d09eb0ab13cf239ede4b018c9f
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-10-25 20:38:30 +0600

    Update Users.js

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,9 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loading from './Shared/Loading/Loading';
 
 const Users = () => {
+    const { data: users, isLoading, refetch } = useQuery(['users'], () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h2 className='text-3xl' > All Users </h2>
+            <h2 className='text-3xl' > All Users: {users.length} </h2>
         </div>
     );
 };
